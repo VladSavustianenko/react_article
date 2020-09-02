d784ac2d1632fe2daf343bd6448a8c86bc5c96bf
Vlad Savustianenko <vladsavustianenko@gmail.com>
2020-09-02 15:01:19 +0300

    Updates

---
+++ precache-manifest.96ca8301abc132b47a8f00623fc6e769.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a9aeeefc498db8f3e9dfa241ade035d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0ebb41580dd51017fc25",
+    "url": "./static/js/2.dd291650.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.dd291650.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d9e584dc1040cb186b7",
+    "url": "./static/js/main.e71ef9c3.chunk.js"
+  },
+  {
+    "revision": "994f72ec58672ee8392d",
+    "url": "./static/js/runtime-main.a5fa282b.js"
+  }
+]);
